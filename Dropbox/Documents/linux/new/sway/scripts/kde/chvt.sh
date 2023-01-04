6e851f8a476ed874a4d156b7e39569f16fbeda7e
Marco Verna <60534377+mverna27@users.noreply.github.com>
2023-01-04 14:49:37 +0100

    Delete Dropbox directory

--- Dropbox/Documents/linux/new/sway/scripts/kde/chvt.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sudo chvt $(ps aux | grep -w "sway" | grep -o tty[0-9] | grep -o [0-9])
--- Dropbox/Documents/linux/new/sway/scripts/openbox/chvt.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sudo chvt $(ps x | grep -w sway | grep -o tty[0-9] | grep -o [0-9])
