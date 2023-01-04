1841aa94a8c284876a8c28cc1075a639ff97e3f2
Marco Verna <60534377+mverna27@users.noreply.github.com>
2023-01-04 14:48:03 +0100

    Delete .config directory

--- .config/waybar/check_otd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# check if opentabletdriver.service is running
-test -z "$(systemctl --user status opentabletdriver.service | grep inactive)" && echo OTD on
--- .config/waybar/check_vrr.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# check if adaptive sync is enabled
-test -z "$(swaymsg -t get_outputs | grep "\"adaptive_sync_status\": \"enabled\",")" || echo VRR on
